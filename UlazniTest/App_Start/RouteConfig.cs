f06c450f33048923f5ce5d070fc8a3fd7d47db76
NDusan88 <dusan-nikolic88@hotmail.com>
2018-09-21 02:22:01 +0200

    Update

--- UlazniTest/App_Start/RouteConfig.cs
+++ UlazniTest/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace UlazniTest
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Proizvod", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ UlazniTest/Controllers/ProizvodController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UlazniTest.Models;
+
+namespace UlazniTest.Controllers
+{
+    public class ProizvodController : Controller
+    {
+        private readonly Test_Baza_Context db = new Test_Baza_Context();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Products.Add(product);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+
+                    ViewBag.Error = e.InnerException;
+                }
+            }
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product proizvod = db.Products.Find(id);
+            if (proizvod == null)
+            {
+                return HttpNotFound();
+            }
+            return View(proizvod
+);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+
+                    ViewBag.Error = e.InnerException;
+                }
+            }
+
+            return View();
+        }
+
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
